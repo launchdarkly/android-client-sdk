ad7010e48e3d05e27c408385f65a5a1b2fa2f0fb
Phillip Wray <phillip.wray@robotsandpencils.com>
2017-09-20 17:32:46 -0600

    Remove unused Util code.

--- launchdarkly-android-client/src/main/java/com/launchdarkly/android/Util.java
+++ launchdarkly-android-client/src/main/java/com/launchdarkly/android/Util.java
@@ -48,16 +48,4 @@ public class Util {
         }
     }
 
-    static synchronized boolean queue(Runnable consumeFunction) {
-        boolean scheduled = false;
-        boolean interruptRunningTask = false;
-        boolean cancelledPreviousHandler = functionHandler.cancel(interruptRunningTask);
-        if (cancelledPreviousHandler || functionHandler.isDone()) {
-            Log.i(TAG, "Put function in queue");
-            functionHandler = functionScheduler.schedule(consumeFunction, LDConfig.DEFAULT_CONNECTION_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
-            scheduled = true;
-        }
-        return scheduled;
-    }
-
 }
