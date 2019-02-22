5766828f455d52e341565bc8569efd114bf0b2d0
Gavin Whelan <gwhelan@launchdarkly.com>
2019-02-22 19:39:00 +0000

    Migration code for upcoming flagstore.

--- launchdarkly-android-client/src/main/java/com/launchdarkly/android/Migration.java
+++ launchdarkly-android-client/src/main/java/com/launchdarkly/android/Migration.java
@@ -4,10 +4,16 @@ import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.gson.JsonObject;
+import com.launchdarkly.android.gson.GsonCache;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 import timber.log.Timber;
 
@@ -16,76 +22,171 @@ class Migration {
     static void migrateWhenNeeded(Application application, LDConfig config) {
         SharedPreferences migrations = application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "migrations", Context.MODE_PRIVATE);
 
+        if (migrations.contains("v2.7.0")) {
+            return;
+        }
+
         if (!migrations.contains("v2.6.0")) {
-            Timber.d("Migrating to v2.6.0 multi-environment shared preferences");
+            migrate_2_7_fresh(application, config);
+        }
 
-            File directory = new File(application.getFilesDir().getParent() + "/shared_prefs/");
-            File[] files = directory.listFiles();
-            ArrayList<String> filenames = new ArrayList<>();
-            for (File file : files) {
-                if (file.isFile())
-                    filenames.add(file.getName());
-            }
+        if (migrations.contains("v2.6.0") && !migrations.contains("v2.7.0")) {
+            migrate_2_7_from_2_6(application);
+        }
+    }
 
-            filenames.remove(LDConfig.SHARED_PREFS_BASE_KEY + "id.xml");
-            filenames.remove(LDConfig.SHARED_PREFS_BASE_KEY + "users.xml");
-            filenames.remove(LDConfig.SHARED_PREFS_BASE_KEY + "version.xml");
-            filenames.remove(LDConfig.SHARED_PREFS_BASE_KEY + "active.xml");
-            filenames.remove(LDConfig.SHARED_PREFS_BASE_KEY + "summaryevents.xml");
-            filenames.remove(LDConfig.SHARED_PREFS_BASE_KEY + "migrations.xml");
+    private static void migrate_2_7_fresh(Application application, LDConfig config) {
+        Timber.d("Migrating to v2.7.0 shared preferences store");
 
-            Iterator<String> nameIter = filenames.iterator();
-            while (nameIter.hasNext()) {
-                String name = nameIter.next();
-                if (!name.startsWith(LDConfig.SHARED_PREFS_BASE_KEY) || !name.endsWith(".xml")) {
-                    nameIter.remove();
-                    continue;
+        ArrayList<String> userKeys = getUserKeysPre_2_6(application, config);
+        SharedPreferences versionSharedPrefs = application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "version", Context.MODE_PRIVATE);
+        Map<String, ?> flagData = versionSharedPrefs.getAll();
+        Set<String> flagKeys = flagData.keySet();
+        JsonObject jsonFlags = GsonCache.getGson().toJsonTree(flagData).getAsJsonObject();
+
+        boolean allSuccess = true;
+        for (Map.Entry<String, String> mobileKeys : config.getMobileKeys().entrySet()) {
+            String mobileKey = mobileKeys.getValue();
+            boolean users = copySharedPreferences(application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "users", Context.MODE_PRIVATE),
+                    application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + mobileKey + "-users", Context.MODE_PRIVATE));
+            boolean stores = true;
+            for (String key : userKeys) {
+                Map<String, ?> flagValues = application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + key, Context.MODE_PRIVATE).getAll();
+                SharedPreferences.Editor userFlagStoreEditor = application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + mobileKey + key + "-flags", Context.MODE_PRIVATE).edit();
+                for (String flagKey : flagKeys) {
+                    Object value = flagValues.get(flagKey);
+                    JsonObject flagJson = jsonFlags.get(flagKey).getAsJsonObject();
+                    flagJson.addProperty("key", flagKey);
+                    flagJson.addProperty("value", GsonCache.getGson().toJson(value));
+                    userFlagStoreEditor.putString(flagKey, GsonCache.getGson().toJson(flagJson));
                 }
-                for (String mobileKey : config.getMobileKeys().values()) {
-                    if (name.contains(mobileKey)) {
-                        nameIter.remove();
-                        break;
-                    }
-                }
+                stores = stores && userFlagStoreEditor.commit();
             }
+            allSuccess = allSuccess && users && stores;
+        }
 
-            ArrayList<String> userKeys = new ArrayList<>();
-            for (String filename : filenames) {
-                userKeys.add(filename.substring(LDConfig.SHARED_PREFS_BASE_KEY.length(), filename.length() - 4));
-            }
-
-            boolean allSuccess = true;
-            for (Map.Entry<String, String> mobileKeys : config.getMobileKeys().entrySet()) {
-                String mobileKey = mobileKeys.getValue();
-                boolean users = copySharedPreferences(application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "users", Context.MODE_PRIVATE),
-                        application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + mobileKey + "-users", Context.MODE_PRIVATE));
-                boolean version = copySharedPreferences(application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "version", Context.MODE_PRIVATE),
-                        application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + mobileKey + "-version", Context.MODE_PRIVATE));
-                boolean active = copySharedPreferences(application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "active", Context.MODE_PRIVATE),
-                        application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + mobileKey + "-active", Context.MODE_PRIVATE));
-                boolean stores = true;
+        if (allSuccess) {
+            Timber.d("Migration to v2.7.0 shared preferences store successful");
+            SharedPreferences migrations = application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "migrations", Context.MODE_PRIVATE);
+            boolean logged = migrations.edit().putString("v2.7.0", "v2.7.0").commit();
+            if (logged) {
+                application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "users", Context.MODE_PRIVATE).edit().clear().apply();
+                application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "version", Context.MODE_PRIVATE).edit().clear().apply();
+                application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "active", Context.MODE_PRIVATE).edit().clear().apply();
+                application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "summaryevents", Context.MODE_PRIVATE).edit().clear().apply();
                 for (String key : userKeys) {
-                    boolean store = copySharedPreferences(application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + key, Context.MODE_PRIVATE),
-                            application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + mobileKey + key + "-user", Context.MODE_PRIVATE));
-                    stores = stores && store;
+                    application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + key, Context.MODE_PRIVATE).edit().clear().apply();
                 }
-                allSuccess = allSuccess && users && version && active && stores;
+            }
+        }
+    }
+
+    private static void migrate_2_7_from_2_6(Application application) {
+        Timber.d("Migrating to v2.7.0 shared preferences store");
+
+        Multimap<String, String> keyUsers = getUserKeys_2_6(application);
+
+        boolean allSuccess = true;
+        for (String mobileKey : keyUsers.keySet()) {
+            SharedPreferences versionSharedPrefs = application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + mobileKey + "-version", Context.MODE_PRIVATE);
+            Map<String, ?> flagData = versionSharedPrefs.getAll();
+            Set<String> flagKeys = flagData.keySet();
+            JsonObject jsonFlags = new JsonObject();
+            for (Map.Entry<String, ?> flagDataEntry : flagData.entrySet()) {
+                JsonObject jsonVal = GsonCache.getGson().fromJson((String)flagDataEntry.getValue(), JsonObject.class);
+                jsonFlags.add(flagDataEntry.getKey(), jsonVal);
             }
 
-            if (allSuccess) {
-                Timber.d("Migration to v2.6.0 multi-environment shared preferences successful");
-                boolean logged = migrations.edit().putString("v2.6.0", "v2.6.0").commit();
-                if (logged) {
-                    application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "users", Context.MODE_PRIVATE).edit().clear().apply();
-                    application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "version", Context.MODE_PRIVATE).edit().clear().apply();
-                    application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "active", Context.MODE_PRIVATE).edit().clear().apply();
-                    application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "summaryevents", Context.MODE_PRIVATE).edit().clear().apply();
-                    for (String key : userKeys) {
-                        application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + key, Context.MODE_PRIVATE).edit().clear().apply();
+            for (String key : keyUsers.get(mobileKey)) {
+                Map<String, ?> flagValues = application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + mobileKey + key + "-user", Context.MODE_PRIVATE).getAll();
+                SharedPreferences.Editor userFlagStoreEditor = application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + mobileKey + key + "-flags", Context.MODE_PRIVATE).edit();
+                for (String flagKey : flagKeys) {
+                    Object value = flagValues.get(flagKey);
+                    JsonObject flagJson = jsonFlags.get(flagKey).getAsJsonObject();
+                    flagJson.addProperty("key", flagKey);
+                    flagJson.addProperty("value", GsonCache.getGson().toJson(value));
+                    userFlagStoreEditor.putString(flagKey, GsonCache.getGson().toJson(flagJson));
+                }
+                allSuccess = allSuccess && userFlagStoreEditor.commit();
+            }
+        }
+
+        if (allSuccess) {
+            Timber.d("Migration to v2.7.0 shared preferences store successful");
+            SharedPreferences migrations = application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + "migrations", Context.MODE_PRIVATE);
+            boolean logged = migrations.edit().putString("v2.7.0", "v2.7.0").commit();
+            if (logged) {
+                for (String mobileKey : keyUsers.keySet()) {
+                    application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + mobileKey + "-version", Context.MODE_PRIVATE).edit().clear().apply();
+                    application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + mobileKey + "-active", Context.MODE_PRIVATE).edit().clear().apply();
+                    for (String key : keyUsers.get(mobileKey)) {
+                        application.getSharedPreferences(LDConfig.SHARED_PREFS_BASE_KEY + mobileKey + key + "-user", Context.MODE_PRIVATE).edit().clear().apply();
                     }
                 }
             }
         }
+    }
+
+    private static ArrayList<String> getUserKeysPre_2_6(Application application, LDConfig config) {
+        File directory = new File(application.getFilesDir().getParent() + "/shared_prefs/");
+        File[] files = directory.listFiles();
+        ArrayList<String> filenames = new ArrayList<>();
+        for (File file : files) {
+            if (file.isFile())
+                filenames.add(file.getName());
+        }
+
+        filenames.remove(LDConfig.SHARED_PREFS_BASE_KEY + "id.xml");
+        filenames.remove(LDConfig.SHARED_PREFS_BASE_KEY + "users.xml");
+        filenames.remove(LDConfig.SHARED_PREFS_BASE_KEY + "version.xml");
+        filenames.remove(LDConfig.SHARED_PREFS_BASE_KEY + "active.xml");
+        filenames.remove(LDConfig.SHARED_PREFS_BASE_KEY + "summaryevents.xml");
+        filenames.remove(LDConfig.SHARED_PREFS_BASE_KEY + "migrations.xml");
+
+        Iterator<String> nameIter = filenames.iterator();
+        while (nameIter.hasNext()) {
+            String name = nameIter.next();
+            if (!name.startsWith(LDConfig.SHARED_PREFS_BASE_KEY) || !name.endsWith(".xml")) {
+                nameIter.remove();
+                continue;
+            }
+            for (String mobileKey : config.getMobileKeys().values()) {
+                if (name.contains(mobileKey)) {
+                    nameIter.remove();
+                    break;
+                }
+            }
+        }
+
+        ArrayList<String> userKeys = new ArrayList<>();
+        for (String filename : filenames) {
+            userKeys.add(filename.substring(LDConfig.SHARED_PREFS_BASE_KEY.length(), filename.length() - 4));
+        }
+        return userKeys;
+    }
+
+    private static Multimap<String, String> getUserKeys_2_6(Application application) {
+        File directory = new File(application.getFilesDir().getParent() + "/shared_prefs/");
+        File[] files = directory.listFiles();
+        ArrayList<String> filenames = new ArrayList<>();
+        for (File file : files) {
+            String name = file.getName();
+            if (file.isFile() && name.startsWith(LDConfig.SHARED_PREFS_BASE_KEY) && name.endsWith("-user.xml")) {
+                filenames.add(file.getName());
+            }
+        }
+
+        Multimap<String, String> keyUserMap = HashMultimap.create();
+        for (String filename : filenames) {
+            Timber.d("Finding keys for file %s", filename);
+            String strip = filename.substring(LDConfig.SHARED_PREFS_BASE_KEY.length(), filename.length() - 9);
+            int splitAt = strip.length() - 44;
+            String mobileKey = strip.substring(0, splitAt);
+            String userKey = strip.substring(splitAt);
+            Timber.d("mobile key: %s user key %s", mobileKey, userKey);
+            keyUserMap.put(mobileKey, userKey);
+        }
+        return keyUserMap;
     }
 
     private static boolean copySharedPreferences(SharedPreferences oldPreferences, SharedPreferences newPreferences) {
