221527594dd632322aaf7edafa0b5edff719132e
Ben Woskow <48036130+bwoskow-ld@users.noreply.github.com>
2019-04-30 11:57:02 -0700

    rename repo and package name and apply markdown templates (#121)

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This script updates the version for the android-client library and releases the artifact + javadoc
+# This script updates the version for the android-client-sdk library and releases the artifact + javadoc
 # It will only work if you have the proper credentials set up in ~/.gradle/gradle.properties
 
 # It takes exactly one argument: the new version.
@@ -9,19 +9,15 @@
 # When done you should commit and push the changes made.
 
 set -uxe
-echo "Starting android-client release."
+echo "Starting android-client-sdk release."
 
 VERSION=$1
 
 # Update version in gradle.properties file:
-sed -i.bak "s/version[ ]*=.*$/version = '${VERSION}'/" launchdarkly-android-client/build.gradle
-rm -f launchdarkly-android-client/build.gradle.bak
-
-# Update version in README.md:
-sed -i.bak "s/com.launchdarkly:launchdarkly-android-client:[^']*/com.launchdarkly:launchdarkly-android-client:${VERSION}/" README.md
-rm -f README.md.bak
+sed -i.bak "s/version[ ]*=.*$/version = '${VERSION}'/" launchdarkly-android-client-sdk/build.gradle
+rm -f launchdarkly-android-client-sdk/build.gradle.bak
 
 ./gradlew test sourcesJar javadocJar packageRelease
 ./gradlew uploadArchives closeAndReleaseRepository
 ./gradlew publishGhPages
-echo "Finished android-client release."
+echo "Finished android-client-sdk release."
