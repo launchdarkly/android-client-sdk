1a489927bd0dfef125cdbbe3adb4193a76c0dfdf
Alex Engelberg <aengelberg@launchdarkly.com>
2022-08-01 11:47:58 -0700

    Fix .ldrelease/publish.sh

--- .ldrelease/publish.sh
+++ .ldrelease/publish.sh
@@ -6,4 +6,4 @@ set -ue
 
 # Publish to Sonatype
 echo "Publishing to Sonatype"
-./gradlew publishToSonatype closeAndReleaseRepository || { echo "Gradle publish/release failed" >&2; exit 1; }
+./gradlew publishToSonatypeRepository closeAndReleaseRepository || { echo "Gradle publish/release failed" >&2; exit 1; }
