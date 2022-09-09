b096f8ddc8426af4de864fd07ae0903752562b67
Eli Bishop <eli@launchdarkly.com>
2022-09-09 11:17:55 -0700

    debugging

--- launchdarkly-android-client-sdk/src/main/java/com/launchdarkly/sdk/android/LDClient.java
+++ launchdarkly-android-client-sdk/src/main/java/com/launchdarkly/sdk/android/LDClient.java
@@ -151,7 +151,9 @@ public class LDClient implements LDClientInterface, Closeable {
             LDUtil.ResultCallback<Void> completeWhenCounterZero = new LDUtil.ResultCallback<Void>() {
                 @Override
                 public void onSuccess(Void result) {
+                    getSharedLogger().warn("### LDClient onSuccess");
                     if (initCounter.decrementAndGet() == 0) {
+                        getSharedLogger().warn("### notifying that initialization is complete ");
                         resultFuture.set(newInstances.get(LDConfig.primaryEnvironmentName));
                     }
                 }
