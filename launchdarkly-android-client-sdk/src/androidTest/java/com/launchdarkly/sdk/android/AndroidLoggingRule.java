4faa28db4883a9ce93cc66d09303752d2b70d86d
Eli Bishop <eli@launchdarkly.com>
2022-09-14 11:41:49 -0700

    ensure test log tag name isn't too long

--- launchdarkly-android-client-sdk/src/androidTest/java/com/launchdarkly/sdk/android/AndroidLoggingRule.java
+++ launchdarkly-android-client-sdk/src/androidTest/java/com/launchdarkly/sdk/android/AndroidLoggingRule.java
@@ -10,9 +10,13 @@ import timber.log.Timber;
 
 /**
  * Adding this rule to a test provides an {@link LDLogger} that writes to the device logs, tagged
- * with the name of the current test.
+ * with the name of the current test (although the name may be truncated due to platform limits on
+ * log tag length).
  */
 public class AndroidLoggingRule extends TestWatcher {
+    // This length limit exists in Android API 25 and above.
+    private static final int ANDROID_MAX_LOG_TAG_LENGTH = 23;
+
     public LDLogAdapter logAdapter;
     public String loggerName;
     public LDLogger logger;
@@ -22,6 +26,9 @@ public class AndroidLoggingRule extends TestWatcher {
     protected void starting(Description description) {
         logAdapter = LDAndroidLogging.adapter();
         loggerName = description.getMethodName();
+        if (loggerName.length() > ANDROID_MAX_LOG_TAG_LENGTH) {
+            loggerName = loggerName.substring(0, ANDROID_MAX_LOG_TAG_LENGTH);
+        }
         logger = LDLogger.withAdapter(logAdapter, loggerName);
     }
 }
