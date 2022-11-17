7a4540421106f554fd78c08bb4bd7b2615d65322
Eli Bishop <eli@launchdarkly.com>
2022-11-16 16:39:06 -0800

    rm duplicated lines

--- contract-tests/src/main/java/com/launchdarkly/sdktest/SdkClientEntity.java
+++ contract-tests/src/main/java/com/launchdarkly/sdktest/SdkClientEntity.java
@@ -234,9 +234,6 @@ public class SdkClientEntity {
       if (params.events.flushIntervalMs != null) {
         eventsBuilder.flushIntervalMillis(params.events.flushIntervalMs.intValue());
       }
-      if (params.events.flushIntervalMs != null) {
-        eventsBuilder.flushIntervalMillis(params.events.flushIntervalMs.intValue());
-      }
       if (params.events.globalPrivateAttributes != null) {
         eventsBuilder.privateAttributes(params.events.globalPrivateAttributes);
       }
